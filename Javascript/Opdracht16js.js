9bb852fd7a1203e9fcf037f2519d57bd1cbd0298
Nanayaa <missnanayaa@live.nl>
2020-10-14 13:46:58 +0200

    14-10

---
+++ Javascript/Opdracht16js.js
@@ -0,0 +1,15 @@
+
+var str = "HELLO WORLD";
+
+var res = str.charAt(20);
+concat(input_string1, input_string2[ input_stringN]);
+function myFunction() {
+    var str = "Hello world!";
+    var res = str.slice(0, 5);
+
+    var str = "How are you doing today?";
+    var res = str.split(" ");
+    var str = "Hello world!";
+    var res = str.substr(1, 4);
+
+
